883cb33cfe7a8f459edd8d284a8414d37f6f4576
MAGHC <windowsapple95@gmail.com>
2022-10-26 21:37:42 +0900

    test: test 코드 작성 버튼클릭 이벤트 에따른 변화

--- src/react-test/Color.test.jsx
+++ src/react-test/Color.test.jsx
@@ -1,15 +1,20 @@
 import Color from "./Color";
 
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 
 test("button has correct initial color", () => {
   render(<Color />);
   const colorBtn = screen.getByRole("button", { name: "색상변경" }); //button 과 text 를 테스트한다
 
-  //
+  //스타일확인
   expect(colorBtn).toHaveStyle({ backgroundColor: "red" });
-});
 
-// test("button turns blue when clicked", () => {
-//   render(<Color></Color>);
-// });
+  //버튼클릭
+  fireEvent.click(colorBtn);
+
+  //색상변경 확인
+  expect(colorBtn).toHaveStyle({ backgroundColor: "blue" });
+
+  //텍스트변경 확인
+  expect(colorBtn.textContent).toBe("변경완료");
+});
