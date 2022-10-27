46c477206c6aa7e17a6a4469c3fc50adac9500b4
MAGHC <windowsapple95@gmail.com>
2022-10-27 19:51:16 +0900

    test: color/text test

--- src/react-test/Color.tsx
+++ src/react-test/Color.tsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { useState } from "react";
 
 const Color = () => {
+  const [color, setColor] = useState("red");
+  const [text, setText] = useState("색상변경");
+
+  const newBtnColor = () => {
+    color === "red" ? setColor("blue") : setColor("red");
+    text === "색상변경" ? setText("변경완료") : setText("색상변경");
+  };
+
   return (
     <>
-      <button style={{ backgroundColor: "red" }}>색상변경</button>
+      <button onClick={newBtnColor} style={{ backgroundColor: color }}>
+        {text}
+      </button>
     </>
   );
 };
